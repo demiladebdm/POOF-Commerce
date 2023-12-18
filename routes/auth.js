b6c08cd882c48e6d011f60145dabe7de10ec9951
demiladebdm <demiladefolarin@gmail.com>
2023-12-18 18:12:26 +0100

    Minor Bug Fixes #2

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { UserModel } = require("../models/User/User");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 /**
  * @swagger
